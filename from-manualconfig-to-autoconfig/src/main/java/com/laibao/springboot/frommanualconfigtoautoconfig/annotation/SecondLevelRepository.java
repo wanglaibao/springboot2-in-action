42b32c0aef791441c28f181eea0c5cb33ef30c52
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-11 15:35:47 +0800

    commit pom.xml file info

---
+++ from-manualconfig-to-autoconfig/src/main/java/com/laibao/springboot/frommanualconfigtoautoconfig/annotation/SecondLevelRepository.java
@@ -0,0 +1,15 @@
+package com.laibao.springboot.frommanualconfigtoautoconfig.annotation;
+
+import org.springframework.stereotype.Repository;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@FirstLevelRepository
+public @interface SecondLevelRepository {
+
+    String value() default "";
+
+}
