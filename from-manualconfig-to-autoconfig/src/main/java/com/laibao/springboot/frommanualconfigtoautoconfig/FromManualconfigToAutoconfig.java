1726d35fb0898b9de3c78b524554ba1663245a26
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-11 15:16:56 +0800

    commit pom.xml file info

---
+++ from-manualconfig-to-autoconfig/src/main/java/com/laibao/springboot/frommanualconfigtoautoconfig/FromManualconfigToAutoconfig.java
@@ -0,0 +1,11 @@
+package com.laibao.springboot.frommanualconfigtoautoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromManualconfigToAutoconfig {
+    public static void main(String[] args) {
+        SpringApplication.run(FromManualconfigToAutoconfig.class, args);
+    }
+}
