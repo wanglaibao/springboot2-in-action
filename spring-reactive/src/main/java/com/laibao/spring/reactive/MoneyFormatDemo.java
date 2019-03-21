175746b45dbdeb62c524d743983fcf857815cc4e
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-21 21:02:52 +0800

    commit pom.xml file info

--- spring-reactive/src/main/java/com/laibao/spring/reactive/MoneyFormatDemo.java
+++ spring-reactive/src/main/java/com/laibao/spring/reactive/MoneyFormatDemo.java
@@ -3,10 +3,13 @@ package com.laibao.spring.reactive;
 import com.laibao.spring.reactive.simpleexample.MyMoney;
 
 import java.text.DecimalFormat;
+import java.util.function.Function;
 
 public class MoneyFormatDemo {
     public static void main(String[] args) {
         MyMoney myMoney = new MyMoney(123123);
-        myMoney.printMoney(i -> new DecimalFormat("#,###").format(i));
+        Function<Integer,String> moneyFormat = i -> new DecimalFormat("#,###").format(i);
+        // 函数式接口还可以支持链式操作
+        myMoney.printMoney(moneyFormat.andThen(str -> "人民币"+str));
     }
 }
