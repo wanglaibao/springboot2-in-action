9b7126d6b33f3098a6ab23794f8b094be1f4a159
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-21 10:58:38 +0800

    commit pom.xml file info

---
+++ spring-reactive/src/main/java/com/laibao/spring/reactive/MiniNumberDemo.java
@@ -0,0 +1,27 @@
+package com.laibao.spring.reactive;
+
+import java.util.stream.IntStream;
+
+public class MiniNumberDemo {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {33,55,-55,100,-666};
+
+        int miniValue = Integer.MAX_VALUE;
+        for (int number:numbers) {
+                if (number < miniValue) {
+                    miniValue = number;
+                }
+        }
+        System.out.println("miniValue is "+miniValue);
+
+        // java8
+        IntStream intStream = IntStream.of(numbers);
+        int smallValue = intStream.min().getAsInt();
+        System.out.println("smallValue is "+smallValue);
+
+        int smallValue2 = IntStream.of(numbers).parallel().min().getAsInt();
+        System.out.println("smallValue2 is "+smallValue2);
+    }
+}
