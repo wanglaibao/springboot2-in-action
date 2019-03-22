8696e8c845ce6b00540f46b19e0cf8bc3988668f
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-22 10:31:04 +0800

    commit pom.xml file info

--- spring-reactive/src/main/java/com/laibao/spring/reactive/FunctionInterDemoOne.java
+++ spring-reactive/src/main/java/com/laibao/spring/reactive/FunctionInterDemoOne.java
@@ -1,6 +1,7 @@
 package com.laibao.spring.reactive;
 
 import java.util.function.Consumer;
+import java.util.function.IntConsumer;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 
@@ -10,9 +11,11 @@ public class FunctionInterDemoOne {
         Predicate<Integer> predicate = (Integer a) ->  a > 100;
         IntPredicate intPredicate = (int b) -> b < 10;
         Consumer<String> consumer = (String str) -> System.out.println(str);
+        IntConsumer intConsumer = i -> System.out.println(i);
         //
         System.out.println(predicate.test(1000));
         consumer.accept("我是金戈");
+        intConsumer.accept(1000);
         System.out.println(intPredicate.test(34));
     }
 }
