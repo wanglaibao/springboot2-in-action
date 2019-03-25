92ae2aee1cf6c929317486caeb76a28ee9c44086
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-25 19:37:57 +0800

    commit pom.xml file info

---
+++ spring-reactive/src/main/java/com/laibao/spring/reactive/lambda/streamexample/StreamDemo5.java
@@ -0,0 +1,8 @@
+package com.laibao.spring.reactive.lambda.streamexample;
+
+public class StreamDemo5 {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ spring-reactive/src/main/java/com/laibao/spring/reactive/lambda/streamexample/Student.java
@@ -0,0 +1,60 @@
+package com.laibao.spring.reactive.lambda.streamexample;
+
+public class Student {
+    private String name;
+    private int age;
+    private Gender gender;
+    private Grade grade;
+
+    public Student() {
+    }
+
+    public Student(String name, int age, Gender gender, Grade grade) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", grade=" + grade +
+                '}';
+    }
+}
