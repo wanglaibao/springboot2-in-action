fc55d702e02977c78cd862c6bfef1927fa8c8153
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-25 15:30:31 +0800

    commit pom.xml file info

--- spring-reactive/src/main/java/com/laibao/spring/reactive/lambda/streamexample/StreamDemo4.java
+++ spring-reactive/src/main/java/com/laibao/spring/reactive/lambda/streamexample/StreamDemo4.java
@@ -1,5 +1,8 @@
 package com.laibao.spring.reactive.lambda.streamexample;
 
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -12,8 +15,24 @@ public class StreamDemo4 {
         str.chars().parallel().forEachOrdered(i -> System.out.println((char) i));
         System.out.println();
 
-        Stream.of(str.split(" ")).collect(Collectors.toList()).forEach(System.out::println);
+        Stream.of(str.split(" ")).collect(Collectors.toList()).forEach(System.out::print);
         System.out.println();
-        Stream.of(str.split(" ")).collect(Collectors.toSet()).forEach(System.out::println);
+        Stream.of(str.split(" ")).collect(Collectors.toSet()).forEach(System.out::print);
+
+
+        Optional<String> stringOptional = Stream.of(str.split(" ")).reduce((s1, s2) -> s1 + "||"+s2);
+        stringOptional.ifPresent(s -> System.out.println(s));
+
+
+        String aa  = Stream.of(str.split(" ")).reduce("",(s1, s2) -> s1 + "||"+s2);
+        System.out.println(aa);
+
+
+        Optional<String> maxValue = Stream.of(str.split(" ")).max((s1,s2) -> s1.compareTo(s2));
+        System.out.println("maxValue : "+maxValue.orElse("none"));
+
+
+        OptionalInt optionalInt = new Random().ints().findFirst();
+        System.out.println(optionalInt.orElse(0));
     }
 }
