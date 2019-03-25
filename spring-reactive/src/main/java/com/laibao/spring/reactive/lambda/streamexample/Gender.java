5e95e1b1ed8e45a3fce0902e0c39198cddff1b91
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-25 19:37:35 +0800

    commit pom.xml file info

---
+++ spring-reactive/src/main/java/com/laibao/spring/reactive/lambda/streamexample/Gender.java
@@ -0,0 +1,5 @@
+package com.laibao.spring.reactive.lambda.streamexample;
+
+public enum Gender {
+    MALE,FEMALE
+}
---
+++ spring-reactive/src/main/java/com/laibao/spring/reactive/lambda/streamexample/Grade.java
@@ -0,0 +1,5 @@
+package com.laibao.spring.reactive.lambda.streamexample;
+
+public enum Grade {
+    ONE,TWO,THREE,FOUR,FIVE,SIX
+}
