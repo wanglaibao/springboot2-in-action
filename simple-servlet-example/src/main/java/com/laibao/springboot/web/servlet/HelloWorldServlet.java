01f283197f9f559146dabf2fec1021c3fd0547ba
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-30 19:23:13 +0800

    commit pom.xml file info

--- simple-servlet-example/src/main/java/com/laibao/springboot/web/servlet/HelloWorldServlet.java
+++ simple-servlet-example/src/main/java/com/laibao/springboot/web/servlet/HelloWorldServlet.java
@@ -6,12 +6,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @WebServlet(urlPatterns = "/my/servlet")
 public class HelloWorldServlet extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("this is the hello world servlet example");
+        long time1 = System.currentTimeMillis();
+        for(int i=0;i<1000;i++) {
+            resp.getWriter().println("this is the hello world servlet example");
+        }
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        long time2 = System.currentTimeMillis();
+        System.err.println("一共耗时: "+ (time2 - time1) / 1000+"秒");
     }
 }
