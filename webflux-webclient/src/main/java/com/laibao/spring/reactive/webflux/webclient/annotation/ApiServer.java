cf00415509ca5aba829b2338db836414d55f91cc
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-04-01 02:28:41 +0800

    commit pom.xml file info

---
+++ webflux-webclient/src/main/java/com/laibao/spring/reactive/webflux/webclient/annotation/ApiServer.java
@@ -0,0 +1,12 @@
+package com.laibao.spring.reactive.webflux.webclient.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApiServer {
+    String value() default "";
+}
