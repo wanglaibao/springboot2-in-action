4a71b923c6a92686d4783621b26ff2051a05eeb9
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-04-01 11:27:18 +0800

    commit pom.xml file info

---
+++ webflux-webclient/src/main/java/com/laibao/spring/reactive/webflux/webclient/interfaces/RestHandler.java
@@ -0,0 +1,26 @@
+package com.laibao.spring.reactive.webflux.webclient.interfaces;
+
+import com.laibao.spring.reactive.webflux.webclient.beans.MethodInfo;
+import com.laibao.spring.reactive.webflux.webclient.beans.ServerInfo;
+
+/**
+ * REST请求调用handler
+ */
+public interface RestHandler {
+    //Object invokeRest(ServerInfo serverInfo, MethodInfo methodInfo);
+    /**
+     * 调用REST请求返回结果
+     * @param methodInfo
+     * @return Object
+     */
+    Object invokeRest(MethodInfo methodInfo);
+
+    /**
+     *
+     * 初始化服务器信息
+     * @param serverInfo
+     */
+    default void init(ServerInfo serverInfo) {
+
+    }
+}
--- webflux-webclient/src/main/java/com/laibao/spring/reactive/webflux/webclient/proxy/JDKProxyCreator.java
+++ webflux-webclient/src/main/java/com/laibao/spring/reactive/webflux/webclient/proxy/JDKProxyCreator.java
@@ -4,6 +4,8 @@ import com.laibao.spring.reactive.webflux.webclient.annotation.ApiServer;
 import com.laibao.spring.reactive.webflux.webclient.beans.MethodInfo;
 import com.laibao.spring.reactive.webflux.webclient.beans.ServerInfo;
 import com.laibao.spring.reactive.webflux.webclient.interfaces.ProxyCreator;
+import com.laibao.spring.reactive.webflux.webclient.interfaces.RestHandler;
+import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
@@ -12,13 +14,21 @@ import java.lang.reflect.Proxy;
 /**
  * 使用JDK动态代理来实现代理类
  */
+@Slf4j
 public class JDKProxyCreator implements ProxyCreator {
 
     @Override
     public Object createProxy(Class<?> clazz) {
-
+        log.info("createProxy : "+clazz);
         //1: 根据接口得到API服务器信息
         ServerInfo serverInfo = extractServerInfo(clazz);
+
+        log.info("serverInfo :  "+serverInfo);
+        //给每一个代理类实现一个REST请求处理器
+        RestHandler restHandler = null;
+
+        //初始化服务器信息(初始化WebClient信息)
+
 
         //2: 创建动态代理对象
         Object proxyObj = Proxy.newProxyInstance(this.getClass().getClassLoader(),
@@ -28,9 +38,9 @@ public class JDKProxyCreator implements ProxyCreator {
                                                         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                                                            //3: 根据方法和参数得到调用方法的信息
                                                             MethodInfo methodInfo = extractMethodInfo(method,args);
-
+                                                            log.info("methodInfo :  "+methodInfo);
                                                             //4:结合服务器信息和方法信息来调用REST接口
-                                                            return null;
+                                                            return restHandler.invokeRest(methodInfo);
                                                         }
                                                     });
 
