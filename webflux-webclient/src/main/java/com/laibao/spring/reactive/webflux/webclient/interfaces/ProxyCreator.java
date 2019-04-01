8f5114ad679f0213eff4782231e9b93faa06b965
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-04-01 10:17:12 +0800

    commit pom.xml file info

--- webflux-webclient/src/main/java/com/laibao/spring/reactive/webflux/webclient/interfaces/ProxyCreator.java
+++ webflux-webclient/src/main/java/com/laibao/spring/reactive/webflux/webclient/interfaces/ProxyCreator.java
@@ -4,7 +4,11 @@ package com.laibao.spring.reactive.webflux.webclient.interfaces;
  * 创建代理类的接口
  */
 public interface ProxyCreator {
+    /**
+     * 根据接口类型来创建代理类实例对象
+     * @param clazz
+     * @return
+     */
     Object createProxy(Class<?> clazz);
-
     //T createProxy(Class<?> clazz);
 }
