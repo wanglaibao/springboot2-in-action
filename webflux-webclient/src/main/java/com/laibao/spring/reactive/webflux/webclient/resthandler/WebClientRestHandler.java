f833cb3e83f7c29d7e4eefbfc61c7748de163278
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-04-01 15:32:24 +0800

    commit pom.xml file info

--- webflux-webclient/src/main/java/com/laibao/spring/reactive/webflux/webclient/resthandler/WebClientRestHandler.java
+++ webflux-webclient/src/main/java/com/laibao/spring/reactive/webflux/webclient/resthandler/WebClientRestHandler.java
@@ -4,9 +4,12 @@ import com.laibao.spring.reactive.webflux.webclient.beans.MethodInfo;
 import com.laibao.spring.reactive.webflux.webclient.beans.ServerInfo;
 import com.laibao.spring.reactive.webflux.webclient.interfaces.RestHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class WebClientRestHandler implements RestHandler{
+
+    private WebClient webClient;
 
     @Override
     public Object invokeRest(MethodInfo methodInfo) {
@@ -15,6 +18,6 @@ public class WebClientRestHandler implements RestHandler{
 
     @Override
     public void init(ServerInfo serverInfo) {
-
+        this.webClient = WebClient.create(serverInfo.getUrl());
     }
 }
